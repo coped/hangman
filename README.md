fe7ffcdba98f589552cba80f8c5411629e74daa8
coped <dennisaaroncope@gmail.com>
2019-04-16 15:48:26 -0700

    Added a feature that allows user to save game.
    This was done by adding a module to the program that provides general, basic serialization methods to the classes that include it.
    
    Upon saving, the program serializes the the instance variables of the game object (the game data in this case), creates a save file, and stores the serialized string within the save file.
    
    Upon execution of the program, if a save file is present, the user will be prompted if they want to load their previous game. The serialized string is loaded from the save file, deserialized, and a new game object is created with the save data, allowing the user to start where they left off.

--- README.md
+++ README.md
@@ -4,14 +4,17 @@
 This is a command line recreation of the game Hangman.
 
 ### Getting Started
-If you have Ruby installed, you can run this game from your own terminal by downloading this repository, navigating to its directory, and running `ruby main.rb`.
+If you have Ruby installed, you can run this game from your own terminal by downloading this repository, navigating to its directory, and running: 
+```
+ruby main.rb
+```
 
 Alternatively, the program can be run through this online [Repl.it IDE here](https://repl.it/@coped/hangman).
 
 If you don't have Ruby but would like to install it, you can find instructions for installation [here](https://www.ruby-lang.org/en/downloads/).
 
 ### How To Play
-The rules for playing hangman can be found [here](https://en.wikipedia.org/wiki/Hangman_(game)).
+The rules for playing Hangman can be found [here](https://en.wikipedia.org/wiki/Hangman_(game)).
 
 # Author
 Dennis Cope, [GITHUB](https://github.com/coped).
--- main.rb
+++ main.rb
@@ -1,7 +1,32 @@
+require 'json'
+
+module Serialization
+
+    @@serializer = JSON
+
+    def serialize
+        data = {}
+        instance_variables.map do |thing|
+            data[thing] = instance_variable_get(thing)
+        end
+
+        @@serializer.dump data
+    end
+
+    def deserialize string
+        data = @@serializer.parse string
+        data.keys.each do |key|
+            instance_variable_set(key, data[key])
+        end
+    end
+end
+
 class Game
+include Serialization
+
     attr_reader :secret_word, :over
 
-    def initialize
+    def initialize display
         @secret_word = get_word
         @lives_left = 6
         @over = false
@@ -9,11 +34,11 @@ class Game
         @correct_guesses = []
         @incorrect_guesses = []
 
-        display_word_progress
+        display_word_progress if display
     end
 
     public
-    
+
     def check_guess player_guess
         secret_letters = @secret_word.downcase.split("")
         if secret_letters.include? player_guess
@@ -32,20 +57,10 @@ class Game
         puts @win? "\nCongratulations, you guessed the word!" : "\nYou ran out of lives!"
     end
 
-    private
-
-    def get_word
-        word = ''
-        until word.length >= 5 && word.length <= 12
-            word = File.readlines('5desk.txt')[rand(61406)].strip
-        end
-        word
-    end
-
     def display_word_progress
         puts "_____________________________________________"
         puts ""
-        puts "Lives left: #{@lives_left}" unless @lives_left > 5
+        puts "Lives left: #{@lives_left}"
         puts "Incorrect guesses: #{@incorrect_guesses}" unless @incorrect_guesses == []
 
         word_progress = @secret_word.split("").map do |letter|
@@ -60,14 +75,24 @@ class Game
         puts ""
         puts word_progress.join("  ")
     end
+    
+    private
+    
+    def get_word
+        word = ''
+        until word.length >= 5 && word.length <= 12
+            word = File.readlines('5desk.txt')[rand(61406)].strip
+        end
+        word
+    end
 end
 
 class Player
-
     def make_guess
+        puts "\nTo save and exit, type 'save'"
         puts "\nGuess a letter:"
         guess = gets.chomp.downcase
-        until guess.length == 1
+        until guess.length == 1 || guess == "save"
             puts "\nYou can only guess one letter at a time."
             puts "Guess a letter:"
             guess = gets.chomp.downcase
@@ -76,15 +101,36 @@ class Player
     end
 end
 
+if File.exists? "hangman-save.txt"
+    puts "Want to load the last saved game? (y/n)"
+    input = gets.chomp.downcase.strip
+end
+
 puts "\nThe computer has chosen a randomly selected word and hidden it."
 puts "\nTry to guess the computer's secret word before running out of lives."
 
-game = Game.new
+if input == "y"
+    game = Game.new false
+    game.deserialize File.open("hangman-save.txt", "r"){|file| file.read}
+    game.display_word_progress
+else 
+    game = Game.new true
+end
 player = Player.new
 
 until game.over
     guess = player.make_guess
+
+    break if guess == "save"
+
     game.check_guess guess
 end
-game.end_result
-puts "The word was: #{game.secret_word}"
+
+if game.over
+    game.end_result
+    puts "The word was: #{game.secret_word}"
+else
+    save = game.serialize
+    File.open("hangman-save.txt", "w"){|file| file.puts save}
+    puts "\nGame saved."
+end
